a89e12beb2d286645839a79ede6bda2b84e74722
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-23 08:43:22 +0200

    header & filters fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,36 +1,37 @@
 import Loader from 'components/Loader/Loader';
 import { Suspense } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import classNames from 'classnames';
 import css from './Layout.module.css';
 
 const Layout = () => {
   return (
-    <div className={css.container}>
+    <>
       <header className={css.header}>
-        <div>
-          <div>
-            <nav>
-              <ul className={css.nav_list}>
-                <li className={css.nav_list_item}>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li className={css.nav_list_item}>
-                  <NavLink to="/catalog">Catalog</NavLink>
-                </li>
-                <li className={css.nav_list_item}>
-                  <NavLink to="/favorites">Favorites</NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
+        <div className={css.container}>
+          <nav>
+            <ul className={css.nav_list}>
+              <li className={css.nav_list_item}>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className={css.nav_list_item}>
+                <NavLink to="/catalog">Catalog</NavLink>
+              </li>
+              <li className={css.nav_list_item}>
+                <NavLink to="/favorites">Favorites</NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
       <main className={css.main}>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        <div className={classNames(css.container, css.main_container)}>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </div>
       </main>
-    </div>
+    </>
   );
 };
 
