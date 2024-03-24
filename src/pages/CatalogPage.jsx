0928b4346a8a26124000e09b908e68a84ba2b581
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-24 20:06:36 +0200

    some fixes

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Catalog from 'components/Catalog/Catalog';
 import Filters from 'components/Filters/Filters';
-import { getCampers, getCampersByPage } from '../redux/campers/campersApi';
+import { getCampers } from '../redux/campers/campersApi';
 // import { useCampers } from 'hooks/useCampers';
 // import allCampers from '../db/campers.json';
 
 const CatalogPage = () => {
   // const [page, setPage] = useState(1);
   const dispatch = useDispatch();
-
-  // let isLoadMore = true;
-
-  // const allCampers = useCampers();
-  // if (page > allCampers.length / 4) isLoadMore = false;
 
   useEffect(() => {
     dispatch(getCampers());
