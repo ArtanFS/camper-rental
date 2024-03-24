7764a9b5686f30f25094f9873dbf2190d0cf28e6
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-24 11:44:40 +0200

    fix load more

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,10 +2,34 @@
 import Button from 'components/Button/Button';
 import CamperCard from 'components/CamperCard/CamperCard';
 import css from './Catalog.module.css';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCampersByPage } from '../../redux/campers/campersApi';
 import campers from '../../db/campers.json';
+import { useCampers } from 'hooks/useCampers';
+// import allCampers from '../../db/campers.json';
 
-const Catalog = ({ loadMore, isLoadMore }) => {
+const Catalog = () => {
+  const [page, setPage] = useState(1);
+  const [isLoadMore, setIisLoadMore] = useState(true);
+
+  const dispatch = useDispatch();
   // const campers = useShownCampers();
+  const allCampers = useCampers();
+
+  useEffect(() => {
+    dispatch(getCampersByPage(page));
+  }, [dispatch, page]);
+
+  // if (page > allCampers.length / 4) isLoadMore = false;
+
+  useEffect(() => {
+    if (page > allCampers.length / 4) setIisLoadMore(false);
+  }, [page, allCampers]);
+
+  const getMoreCampers = () => {
+    setPage(prev => prev + 1);
+  };
 
   return (
     <section className={css.catalog_section}>
@@ -16,7 +40,7 @@ const Catalog = ({ loadMore, isLoadMore }) => {
       {campers.length > 0 && (
         <Button
           className={css.loadmore_btn}
-          onClick={loadMore}
+          onClick={getMoreCampers}
           disabled={!isLoadMore}
         >
           {isLoadMore ? 'Load more' : 'No more campers'}
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -4,33 +4,36 @@ import Catalog from 'components/Catalog/Catalog';
 import Filters from 'components/Filters/Filters';
 import { getCampers, getCampersByPage } from '../redux/campers/campersApi';
 // import { useCampers } from 'hooks/useCampers';
-import allCampers from '../db/campers.json';
+// import allCampers from '../db/campers.json';
 
 const CatalogPage = () => {
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   const dispatch = useDispatch();
 
-  let isLoadMore = true;
+  // let isLoadMore = true;
 
   // const allCampers = useCampers();
-  if (page > allCampers.length / 4) isLoadMore = false;
+  // if (page > allCampers.length / 4) isLoadMore = false;
 
   useEffect(() => {
     dispatch(getCampers());
   }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(getCampersByPage(page));
-  }, [dispatch, page]);
+  // useEffect(() => {
+  //   dispatch(getCampersByPage(page));
+  // }, [dispatch, page]);
 
-  const getMoreCampers = () => {
-    setPage(prev => prev + 1);
-  };
+  // const getMoreCampers = () => {
+  //   setPage(prev => prev + 1);
+  // };
 
   return (
     <>
       <Filters />
-      <Catalog loadMore={getMoreCampers} isLoadMore={isLoadMore} />
+      <Catalog
+      // loadMore={getMoreCampers}
+      // isLoadMore={isLoadMore}
+      />
     </>
   );
 };
