7204cebb5927dc65e5b77baac459d80b5fd0211b
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-24 12:03:32 +0200

    add to favPage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,15 +1,24 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { deleteShownCampers } from '../redux/campers/campersSlice';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { deleteShownCampers } from '../redux/campers/campersSlice';
+
+import FavCampers from 'components/FavCampers/FavCampers';
+
+// import { useFavCampers } from 'hooks/useFavCampers';
 
 const FavoritesPage = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  // const favCampers = useFavCampers();
 
-  useEffect(() => {
-    dispatch(deleteShownCampers());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(deleteShownCampers());
+  // }, [dispatch]);
 
-  return <div>FavoritesPage</div>;
+  return (
+    <>
+      <FavCampers />
+    </>
+  );
 };
 
 export default FavoritesPage;
