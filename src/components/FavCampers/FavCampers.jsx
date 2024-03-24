df6f631eac62dbfc5369ff079aaa6ae18ce3e11e
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-24 23:19:59 +0200

    fix render

--- src/components/FavCampers/FavCampers.jsx
+++ src/components/FavCampers/FavCampers.jsx
@@ -1,21 +1,9 @@
-// import { useShownCampers } from 'hooks/useShownCampers';
-// import Button from 'components/Button/Button';
+import { useFavCampers } from 'hooks/useFavCampers';
 import CamperCard from 'components/CamperCard/CamperCard';
 import css from './FavCampers.module.css';
-// import { useEffect, useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { getCampersByPage } from '../../redux/campers/campersApi';
-// import campers from '../../db/campers.json';
-// import { useCampers } from 'hooks/useCampers';
-import { useFavCampers } from 'hooks/useFavCampers';
-// import allCampers from '../../db/campers.json';
 
 const FavCampers = () => {
-  // const dispatch = useDispatch();
-  // const campers = useShownCampers();
   const favCampers = useFavCampers();
-
-  // if (page > allCampers.length / 4) isLoadMore = false;
 
   return (
     <section className={css.catalog_section}>
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,18 +1,14 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { deleteShownCampers } from '../redux/campers/campersSlice';
-
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteShownCampers } from '../redux/campers/campersSlice';
 import FavCampers from 'components/FavCampers/FavCampers';
 
-// import { useFavCampers } from 'hooks/useFavCampers';
-
 const FavoritesPage = () => {
-  // const dispatch = useDispatch();
-  // const favCampers = useFavCampers();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(deleteShownCampers());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(deleteShownCampers());
+  }, [dispatch]);
 
   return (
     <>
