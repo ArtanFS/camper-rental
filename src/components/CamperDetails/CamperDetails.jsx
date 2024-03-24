dd6f96bfdfe8013bf477cc66e3e8e01a47c295ca
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-24 03:06:19 +0200

    scroll card info

--- src/components/CamperDetails/CamperDetails.jsx
+++ src/components/CamperDetails/CamperDetails.jsx
@@ -5,6 +5,7 @@ import Price from 'components/Price/Price';
 import { Link, Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Loader from 'components/Loader/Loader';
+// import classNames from 'classnames';
 
 const CamperDetails = ({ data }) => {
   return (
@@ -24,30 +25,36 @@ const CamperDetails = ({ data }) => {
         />
       </div>
       <Price className={css.card_title} price={data.price} />
-      <ul className={css.card_img_wrap}>
-        {data.gallery.length > 0 &&
-          data.gallery.map(img => (
-            <li key={data._id} className={css.card_img_item}>
-              <img
-                className={css.card_img}
-                src={img}
-                alt=""
-                width={290}
-                height={310}
-              />
-            </li>
-          ))}
-      </ul>
-      <p className={css.card_description}>{data.description}</p>
-      <div>
-        <ul>
-          <li>
-            <Link to="features">Features</Link>
-          </li>
-          <li>
-            <Link to="reviews">Reviews</Link>
-          </li>
-        </ul>
+      <div className={css.scroll_container_wrap}>
+        <div className={css.scroll_container}>
+          <div className={css.card_info}>
+            <ul className={css.card_img_wrap}>
+              {data.gallery.length > 0 &&
+                data.gallery.map(img => (
+                  <li key={data._id} className={css.card_img_item}>
+                    <img
+                      className={css.card_img}
+                      src={img}
+                      alt=""
+                      width={290}
+                      height={310}
+                    />
+                  </li>
+                ))}
+            </ul>
+            <p className={css.card_description}>{data.description}</p>
+            <div>
+              <ul>
+                <li>
+                  <Link to="features">Features</Link>
+                </li>
+                <li>
+                  <Link to="reviews">Reviews</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
       <Suspense fallback={<Loader />}>
         <Outlet />
