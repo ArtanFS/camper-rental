93ea02732372125f22f76871d187fdfadea38821
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-26 13:30:45 +0200

    fix reload page v2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,16 @@
 import Loader from 'components/Loader/Loader';
 import { Suspense, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import css from './Layout.module.css';
 import Header from 'components/Header/Header';
 
 const Layout = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
     const isPageRefreshed =
       performance.getEntriesByType('navigation')[0].type === 'reload';
 
-    if (isPageRefreshed) navigate('/campersrent');
+    if (isPageRefreshed) window.location.replace('/campersrent');
+    // window.location.replace('/home');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
