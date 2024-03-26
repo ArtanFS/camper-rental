2b1720a653cc1fea2ae393edddfac1f7feba4ac4
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-26 03:31:29 +0200

    go to home by reload page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,20 @@
 import Loader from 'components/Loader/Loader';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import css from './Layout.module.css';
 import Header from 'components/Header/Header';
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isPageRefreshed =
+      performance.getEntriesByType('navigation')[0].type === 'reload';
+
+    if (isPageRefreshed) navigate('/');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Header />
