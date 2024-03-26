8bbcfe51a5d03e8ce296a2f9d95333f48e1e6323
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2024-03-26 14:11:49 +0200

    fix nocampers

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -7,11 +7,12 @@ const Catalog = ({ campers, onClick, isLoadMore, filtered }) => {
   return (
     <section className={css.catalog_section}>
       <ul className={css.card_list}>
-        {campers.length > 0 && campers[0]._id ? (
-          campers.map(camper => <CamperCard key={camper._id} data={camper} />)
-        ) : (
-          <NoCards />
-        )}
+        {campers.length > 0 &&
+          (campers[0]._id ? (
+            campers.map(camper => <CamperCard key={camper._id} data={camper} />)
+          ) : (
+            <NoCards />
+          ))}
       </ul>
       {campers.length > 0 && filtered && (
         <Button
